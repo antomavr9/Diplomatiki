a395f78c377ecba16559f6c8964cafadb3c7bd42
Panteleimonas <57897777+Panteleimonas@users.noreply.github.com>
2023-07-24 13:26:31 +0300

    final touches

--- Program.cs
+++ Program.cs
@@ -112,12 +112,6 @@ class Program
 
         #region Machine
 
-        // TIMER
-        // int t_sec = 1000; // 1 second = 1000 milliseconds
-        // Console.WriteLine("Timer Started!");
-        // await Task.Delay(t_sec*60);
-        // Console.WriteLine("Timer Finished!");
-
         List<AddressUnit> addressUnits = new List<AddressUnit>
         {
             new AddressUnit() {Id = "1", Area = "4X", Address = 1, CommunicationTag = "Add1", DataType = typeof (ushort)}, //Id is mandatory
@@ -126,21 +120,33 @@ class Program
             new AddressUnit() {Id = "4", Area = "4X", Address = 4, CommunicationTag = "Add4", DataType = typeof (ushort)}
         };
 
-        
-
-        // Connect to server through machine api
+        // Create machine object
         ModbusMachine<string, string> machine = new ModbusMachine<string, string>("1", ModbusType.Tcp, "127.0.0.1:502", addressUnits, 1, 0);
-        bool connectionStatusMachine = await machine.ConnectAsync(); // we can also use machine.BaseUtility.ConnectAsync();
         // Here the connection starts and returns a boolean flag which we can use later to check if it connected successfully.
-
+        bool connectionStatusMachine = await machine.ConnectAsync(); // we can also use machine.BaseUtility.ConnectAsync();
         // Using connectAsync is not necessarily needed because machine.SetDatasAsync and machine.GetDatasAsync connect by themselves.
         
-        // // Disconnect
-        // bool disconnectflag = machine.Disconnect();
+        // Disconnect
+        bool disconnectflag = machine.Disconnect();
+        Console.WriteLine("IsConnected:" + machine.IsConnected);
 
-        // // Here we use the GetAddressUnitById function to get a single address unit.
-        // AddressUnit<string> singleaddressunit = machine.GetAddressUnitById("1");
-        // Console.WriteLine("Address Unit"+ singleaddressunit);
+        // Here we use the GetAddressUnitById function to get a single address unit.
+        AddressUnit<string> singleaddressunit = machine.GetAddressUnitById("1");
+        Console.WriteLine($"Address Unit: {singleaddressunit.Id} {singleaddressunit.Area} {singleaddressunit.Address} {singleaddressunit.CommunicationTag}");
+
+        // OR YOU CAN PRINT IT LIKE THIS
+        // if (singleaddressunit != null)
+        // {
+        //     Console.WriteLine($"Address Unit Id: {singleaddressunit.Id}");
+        //     Console.WriteLine($"Address Unit Area: {singleaddressunit.Area}");
+        //     Console.WriteLine($"Address Unit Address: {singleaddressunit.Address}");
+        //     Console.WriteLine($"Address Unit CommunicationTag: {singleaddressunit.CommunicationTag}");
+        //     Console.WriteLine($"Address Unit DataType: {singleaddressunit.DataType}");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Address Unit not found.");
+        // }
 
 
         // // TIMER and IsConnected check.
@@ -167,7 +173,9 @@ class Program
 
             var setDic = new Dictionary<string, double> { { "Add1", (double)Add1 }, { "Add2", (double)Add2 }, { "Add3", (double)Add3 } }; // I can access each address one by one from here
 
-            // ---------------------------------- Set Data ------------------------------------------------- //
+            #region SET DATA
+
+            // ------------------------------------------- SET DATA ------------------------------------------------- //
             var returnSetObject = await machine.SetDatasAsync(MachineDataType.CommunicationTag, setDic);
             // bool SetDatas = returnSetObject.Datas; // this parameter is not needed because SetDatas and the following parameter SetSuccessStatus are the same.
             int SetErrorCode = returnSetObject.ErrorCode;
@@ -185,40 +193,58 @@ class Program
                 Console.WriteLine("Set Data Status: Error Code: " + SetErrorCode+ ". Error Message: " + SetErrorMsg + ".");
             }
 
+            #endregion
+
+            #region GET DATA BY TAG
     
-            // --------------------------------------------------------- GET DATAS BY TAG --------------------------------------------------- //
-            async Task<ReturnStruct<byte[]>?> GetDatasByCommunicationTag(string Tag)
-            {
-                foreach (var addressUnit in addressUnits)
-                {
-                    if (addressUnit.CommunicationTag == Tag)
-                    {
-                        //result.Add(addressUnit);
-                        string address= addressUnit.Area + " " + addressUnit.Address;
-                        Console.WriteLine(address);
-                        ReturnStruct<byte[]> rect = await machine.BaseUtility.GetUtilityMethods<IUtilityMethodDatas>().GetDatasAsync(address, 1);
-                        //byte[] GetDatas = rect.Datas;
-                        return rect;
-                    }
-                }
-                return null;
-            }
+            // // ----------------------------------------- GET DATA BY TAG --------------------------------------------------- //
+            // async Task<ReturnStruct<byte[]>?> GetDatasByCommunicationTag(string Tag)
+            // {
+            //     foreach (var addressUnit in addressUnits)
+            //     {
+            //         if (addressUnit.CommunicationTag == Tag)
+            //         {
+            //             //result.Add(addressUnit);
+            //             string address= addressUnit.Area + " " + addressUnit.Address;
+            //             Console.WriteLine(address);
+            //             ReturnStruct<byte[]> rect = await machine.BaseUtility.GetUtilityMethods<IUtilityMethodDatas>().GetDatasAsync(address, 1);
+            //             //byte[] GetDatas = rect.Datas;
+            //             return rect;
+            //         }
+            //     }
+            //     return null;
+            // }
 
-            ReturnStruct<byte[]>? returnGetObject = await GetDatasByCommunicationTag("Add1");
-            byte[] GetDatas = returnGetObject.Value.Datas;
-
-            // Print Received Data
-            Console.WriteLine("Data Received from Modbus Server:");
-            for (int i = 1; i < GetDatas!.Length; i+=2)
-            {
-                Console.WriteLine(GetDatas[i]);
-                // When the Data are binary
-                // var BinaryForm = Convert.ToString(GetDatas[i], 2);
-                // Console.WriteLine(BinaryForm);                
-            }
+            // ReturnStruct<byte[]>? returnGetObject = await GetDatasByCommunicationTag("Add1");
+            // byte[] GetDatas = returnGetObject.Value.Datas;
 
 
-            // // ------------------------------- Get Data through BaseUtility ------------------------------ //
+            // // Check Success Status
+            // if(returnGetObject.Value.IsSuccess)
+            // {
+            //     Console.WriteLine("Get Data Status: Data Received Succesfully!");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("Get Data Status: Error Code: " + returnGetObject.Value.ErrorCode + ". Error Message: " + returnGetObject.Value.ErrorMsg + ".");
+            // }
+
+
+            // // Print Received Data
+            // Console.WriteLine("Data Received from Modbus Server:");
+            // for (int i = 1; i < GetDatas!.Length; i+=2)
+            // {
+            //     Console.WriteLine(GetDatas[i]);
+            //     // When the Data are binary
+            //     // var BinaryForm = Convert.ToString(GetDatas[i], 2);
+            //     // Console.WriteLine(BinaryForm);                
+            // }
+
+            #endregion
+
+            #region GET DATA THROUGH BASE UTILITY
+
+            // // ------------------------------- GET DATA THROUGH BASEUTILITY ------------------------------ //
             // var returnGetObject = await machine.BaseUtility.GetUtilityMethods<IUtilityMethodDatas>().GetDatasAsync("4X 1", 2);
             // // in the above line, instead of var we might use ReturnStruct<byte[]>
             // byte[]? GetDatas = returnGetObject.Datas;
@@ -246,7 +272,11 @@ class Program
             //     // Console.WriteLine(BinaryForm);                
             // }
 
-            // // -------------------------------- Get Data using machine -------------------------------- //
+            #endregion
+
+            #region GET DATA USING MACHINE
+
+            // // -------------------------------- GET DATA USING MACHINE ------------------------------------------- //
             // var returnGetObject = await machine.GetDatasAsync(MachineDataType.CommunicationTag); 
             // Dictionary<string, ReturnUnit<double>> GetDatas = returnGetObject.Datas; //MapGetValuesToSetValues()  TI KANEI AYTO REEEEEE
             // int GetErrorCode = returnGetObject.ErrorCode;
@@ -272,6 +302,7 @@ class Program
             //     Console.WriteLine("Key: " + key + ", DeviceValue: " + DeviceValue);
             // }
 
+            #endregion
 
             Console.WriteLine("Press anything to exit...");
             Console.ReadKey();
