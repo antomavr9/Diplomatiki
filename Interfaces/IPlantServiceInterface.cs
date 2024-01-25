b93169ebaddfed2b2c9402b17b08fd145163afbb
antomavr9 <antonisira@gmail.com>
2024-01-25 21:35:42 +0200

    Major Changes

--- Interfaces/IPlantServiceInterface.cs
+++ Interfaces/IPlantServiceInterface.cs
@@ -1,12 +1,12 @@
-namespace ModbusExtension;
-public interface IPlantServiceInterface
-{
-    string GetActivePower();
-    string GetReactivePower();
-    string GetVoltageL1();
-    string GetVoltageL2();
-    string GetVoltageL3();
-    string GetDeviceStatus();
-    string GetLogStatus(); 
-    string GetPowerSetPointLevel();
-}
+// namespace ModbusExtension;
+// public interface IPlantServiceInterface
+// {
+//     string GetActivePower();
+//     string GetReactivePower();
+//     string GetVoltageL1();
+//     string GetVoltageL2();
+//     string GetVoltageL3();
+//     string GetDeviceStatus();
+//     string GetLogStatus(); 
+//     string GetPowerSetPointLevel();
+// }
